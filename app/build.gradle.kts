504ed1db4e27415248c5f7e5fb413f93f25a7321
chanakyamittapally <chanakya.mj@gmai.com>
2023-08-16 15:15:46 -0500

    jetpack compose set up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,12 +35,19 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.8"
     }
 }
 
 dependencies {
 
+    val composeDep = platform("androidx.compose:compose-bom:2023.08.00")
+
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation (composeDep)
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -51,4 +58,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(composeDep)
 }
--- app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_practice/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.jetpack_compose_practice
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.jetpack_compose_practice.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.text.text = "sample"
         setContentView(binding.root)
-
     }
+
 }
